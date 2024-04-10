83dbfaa5b7a109201e4addf7559aefd0ee482d8f
HarmanGill <harman97gill@gmail.com>
2024-04-09 21:17:56 -0400

    first route setup, controller racquets started

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
 
+const racquetsController = require('./controller/racquets.js')
+app.use('/racquets', racquetsController)
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
