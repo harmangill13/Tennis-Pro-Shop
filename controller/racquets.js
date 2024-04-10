61d9fdd5bee798f7e521b904fbd8d1c5d5f5c193
HarmanGill <harman97gill@gmail.com>
2024-04-10 17:27:37 -0400

    working on home page, and racquets page

--- controller/racquets.js
+++ controller/racquets.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('list of racquets route')
+  res.render('racquets.ejs')
+})
+
+router.get('/:index', (req, res) => {
+  res.send('hi')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
 
+
+// CONTROLLERS
 const racquetsController = require('./controller/racquets.js')
 app.use('/racquets', racquetsController)
 
+// ROUTES
+app.get('/home', (req, res) => {
+  res.send('hi')
+})
+
+// SERVER CONNECTION
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
